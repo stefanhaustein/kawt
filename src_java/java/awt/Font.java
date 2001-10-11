e45a29f99851d95d4533f37fe5d0eb809103190c
haustein <haustein>
2001-10-11 23:18:01 +0000

    Container.setLayout fixed, Font.isPlain fixed, MIDlet throws fixed

---
+++ src_java/java/awt/Font.java
@@ -0,0 +1,106 @@
+// Font.java
+//
+// 2000-09-08 MK Added new Licensetext
+// 2000-08-06 MIDP changes 
+//      
+//#include ..\..\license.txt
+//
+// kAWT version 0.95
+//
+// Copyright (C) 1999-2000 by Michael Kroll & Stefan Haustein GbR, Essen
+//
+// Contact: kawt@kawt.de
+// General Information about kAWT is available at: http://www.kawt.de
+//
+// Using kAWT for private and educational and in GPLed open source
+// projects is free. For other purposes, a commercial license must be
+// obtained. There is absolutely no warranty for non-commercial use.
+//
+//
+// 1. BECAUSE THE PROGRAM IS LICENSED FREE OF CHARGE, THERE IS NO
+//    WARRANTY FOR THE PROGRAM, TO THE EXTENT PERMITTED BY APPLICABLE
+//    LAW.  EXCEPT WHEN OTHERWISE STATED IN WRITING THE COPYRIGHT
+//    HOLDERS AND/OR OTHER PARTIES PROVIDE THE PROGRAM "AS IS" WITHOUT
+//    WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING, BUT
+//    NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+//    FITNESS FOR A PARTICULAR PURPOSE.  THE ENTIRE RISK AS TO THE
+//    QUALITY AND PERFORMANCE OF THE PROGRAM IS WITH YOU.  SHOULD THE
+//    PROGRAM PROVE DEFECTIVE, YOU ASSUME THE COST OF ALL NECESSARY
+//    SERVICING, REPAIR OR CORRECTION.
+//   
+// 2. IN NO EVENT UNLESS REQUIRED BY APPLICABLE LAW OR AGREED TO IN
+//    WRITING WILL ANY COPYRIGHT HOLDER, OR ANY OTHER PARTY WHO MAY
+//    MODIFY AND/OR REDISTRIBUTE THE PROGRAM AS PERMITTED ABOVE, BE
+//    LIABLE TO YOU FOR DAMAGES, INCLUDING ANY GENERAL, SPECIAL,
+//    INCIDENTAL OR CONSEQUENTIAL DAMAGES ARISING OUT OF THE USE OR
+//    INABILITY TO USE THE PROGRAM (INCLUDING BUT NOT LIMITED TO LOSS OF
+//    DATA OR DATA BEING RENDERED INACCURATE OR LOSSES SUSTAINED BY YOU
+//    OR THIRD PARTIES OR A FAILURE OF THE PROGRAM TO OPERATE WITH ANY
+//    OTHER PROGRAMS), EVEN IF SUCH HOLDER OR OTHER PARTY HAS BEEN
+//    ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.
+//   
+//    END OF TERMS AND CONDITIONS
+// 
+//#endinclude
+
+/** this class stores just a description of a font;
+    the "real" font is stored in FontMetrics (!!) */
+
+
+package java.awt;
+
+public class Font {
+
+    static Font defaultFont = new Font ("plain", Font.PLAIN, 8);
+    
+    static public final int PLAIN = 0;
+    static public final int BOLD = 1;
+    static public final int ITALIC = 2;
+    
+    protected String name;
+    protected int style;
+    protected int size;
+    
+    public Font (String name, int style, int size) {
+	this.name = name;
+        this.style = style;
+	this.size = size;
+    }
+
+    public int getSize () {
+        return size;
+    }
+
+
+    public boolean isBold () {
+        return (style & BOLD) != 0;
+    }
+
+
+    public boolean isItalic() {
+        return (style & ITALIC) != 0;		
+    }
+
+    public boolean isPlain() {
+        return style == PLAIN;		
+    }
+    
+    public String getName() {
+      return name;
+    }
+
+
+    public Font deriveFont (int style) {
+        return new Font (name, style, size);
+    }
+}
+
+/*
+ * $Log: Font.java,v $
+ * Revision 1.5  2001/10/11 23:18:01  haustein
+ * Container.setLayout fixed, Font.isPlain fixed, MIDlet throws fixed
+ *
+ * Revision 1.4  2001/10/08 12:07:51  mkroll
+ * Added isItalic(), isPlain() and getName()
+ *
+ */
